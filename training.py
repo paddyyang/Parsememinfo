a392501f5fb7a699235e50141b13244a941b50b6
Paddy Yang <hygd02@163.com>
2017-04-14 00:28:15 +0800

    debug draw_node

--- training.py
+++ training.py
@@ -8,16 +8,27 @@ import collect_data as codata
 import draw_node as drawn
 
 ex1 = codata.get_training_data('com.pyzed.androidmemoryleaktest', 11)
+print 'ex1 = ' 
 print ex1
 
 # compute delta
 delta = [[0 for i in range(len(ex1[0]))] for j in range(len(ex1)-1)]
+vector = [0 for i in range(len(ex1)-1)]
 
 for i in range(0, len(delta)):
     delta[i][0] = ex1[i+1][0] - ex1[i][0]
     delta[i][1] = ex1[i+1][1] - ex1[i][1]
 
+for i in range(0, len(vector)):
+    if delta[i][1] == 0:
+        vector[i] = float("inf")
+    else:
+        vector[i] = delta[i][0] / delta[i][1]
 
+print 'delta = '
 print delta
+
+print 'vector = '
+print vector
 
 drawn.draw_node(delta)
