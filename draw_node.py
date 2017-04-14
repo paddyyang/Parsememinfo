97f1758b732a144a47e41dfdef74f039f2425af2
ygd <ygd@xx.com>
2017-04-14 11:18:45 +0800

    draw_node function verified OK

--- draw_node.py
+++ draw_node.py
@@ -4,14 +4,5 @@ from numpy import *
 
 def draw_node(data_array):
     matrix_data = mat(data_array)
-    print 'matrix_data = '
-    print matrix_data
-    print 'matrix_data[:,0]: '
-    print matrix_data[:,0]
-    print 'matrix_data[:,1]'
-    print matrix_data[:,1]
-    a=[0,1, 2, 3]
-    b=[4, 5, 6 ,7]
-    #plt.scatter(a,b)
-    plt.scatter(matrix_data[0,:], matrix_data[1,:])
+    plt.scatter(matrix_data[:,0], matrix_data[:,1])
     plt.show()
