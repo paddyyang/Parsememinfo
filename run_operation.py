ec6db7722c78da12c079593969c48331286d1993
ygd <ygd@xx.com>
2017-04-25 16:16:14 +0800

    add restart test operation

--- run_operation.py
+++ run_operation.py
@@ -4,13 +4,44 @@ import subprocess
 import sys
 import math
 
+import utils_top_activity as utop
+
 def rotate_screen(orien):
     op_cmd = "./shell/adb_rotate.sh " + orien
     os.system(op_cmd)
     return
 
+def rotate():
+    while True:
+        rotate_screen('l')
+        time.sleep(2);
+        rotate_screen('p')
 
-while True:
-   rotate_screen('l')
-   time.sleep(2);
-   rotate_screen('p')
+def killPackage(package):
+    op_cmd = "adb shell am force-stop " + package
+    os.system(op_cmd)
+    return
+
+def startPackage(component):
+    op_cmd = "adb shell am start -n " + component
+    os.system(op_cmd)
+    return
+    
+def restart():
+    component = utop.topActivity() 
+    temp = component.split('/')
+    package = temp[0]
+    while True:
+       killPackage(package)
+       time.sleep(3);
+       startPackage(component) 
+       time.sleep(3);
+
+if(len(sys.argv) == 2):
+    option = sys.argv[1]
+    if (option == 'rotate'):
+        rotate()
+    elif (option == 'restart'):
+        restart()
+
+
---
+++ utils_top_activity.py
@@ -0,0 +1,23 @@
+import os
+#
+#  use 'adb shell dumpsys activity top' command
+#  will give us the current activity name
+#
+def execCmd(cmd):
+    lines = os.popen(cmd)
+    text = lines.readlines()
+    lines.close()
+    return text
+
+def getComponent(line_text):
+    
+     line0 = line_text.strip()
+     if line0:
+        temp = line0.split()
+
+     return temp[1]
+
+def topActivity():
+    text = execCmd('adb shell dumpsys activity top')
+    component = getComponent(text[1])
+    return component
